6b9553b506ee74f8f1b8cf63ce8948b434580477
matdwcy <1711837358@qq.com>
2019-09-20 18:04:06 +0800

    fix

--- rw_edgar/rw_xlsx.py
+++ rw_edgar/rw_xlsx.py
@@ -10,6 +10,7 @@ date = pd.read_csv('/home/ubuntu/edgar_scrapy/edgar/edgar/out.xlsx',sep = '~')
 data = []
 for res in date.index.values:
     row = date.ix[res,date.columns.values].to_dict()
+    print(row)
     data.append(row)
     session.execute("""insert into hangseng (constituent_type,constituent_name,code)values(%s ,%s ,%s)""",
                     ('50 xlsx',str(row['constituent_name']),str(row['code'])))
