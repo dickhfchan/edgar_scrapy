d497a5a26ed0bbdddb5e737fefe3919579174cd8
matdwcy <1711837358@qq.com>
2019-09-20 18:10:22 +0800

    fix

--- rw_edgar/rw_xlsx.py
+++ rw_edgar/rw_xlsx.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from cassandra.cluster import Cluster
-# import sys
-# import io
-# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 server = ['52.76.70.227']
 cluster = Cluster(server)
 session = cluster.connect('scrapy')
-date = pd.read_csv('/home/ubuntu/edgar_scrapy/edgar/edgar/out.xlsx',sep = '~')
+date = pd.read_excel('/home/ubuntu/edgar_scrapy/edgar/edgar/out.xlsx')
 data = []
 for res in date.index.values:
     row = date.ix[res,date.columns.values].to_dict()
     print(row)
     data.append(row)
     session.execute("""insert into hangseng (constituent_type,constituent_name,code)values(%s ,%s ,%s)""",
-                    ('50 xlsx',str(row['constituent_name']),str(row['code'])))
+                    ('50 xlsx',str(row['Constituent Name']),str(row['code'])))
 print('out file data',len(data))
