9b525d0eb7d66daf96f846280ce4a194f73740bf
Ubuntu <ubuntu@ip-172-26-12-75.ap-southeast-1.compute.internal>
2019-09-24 09:50:59 +0000

    get codes

--- piyilu.py
+++ piyilu.py
@@ -9,8 +9,7 @@ server = ['52.76.70.227']
 cluster = Cluster(server)
 session = cluster.connect('scrapy')
 result = pd.DataFrame(list(session.execute('select code from hangseng')))
-# codes = [code for code in result]
-print(result)
+codes = [code for code in result['code']]
 data = {
     'category': '0',
     'market': 'SEHK',
