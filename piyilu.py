eb7b62893ef9f68f5d29ff861876ab7aacb74150
matdwcy <1711837358@qq.com>
2019-09-25 16:55:00 +0800

    a

--- piyilu.py
+++ piyilu.py
@@ -83,5 +83,6 @@ for inx,url in enumerate(urls):
             articles += body
         doc.close()
     finally:
-        session.execute("insert into hkexnews (url,date,code,abbreviation,body)values(%s ,%s ,%s, %s, %s)",
-                    (link,dates[inx],codes[inx],abbreviations[inx],articles))
+        print(abbreviations[inx])
+        # session.execute("insert into hkexnews (url,date,code,abbreviation,body)values(%s ,%s ,%s, %s, %s)",
+        #             (link,dates[inx],codes[inx],abbreviations[inx],articles))
