0b467707e6b902bc7638d6b008917b25a279bc69
matdwcy <1711837358@qq.com>
2019-10-09 10:05:52 +0800

    update codes

--- piyilu.py
+++ piyilu.py
@@ -14,9 +14,8 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 server = ['52.76.70.227']
 cluster = Cluster(server)
 session = cluster.connect('scrapy')
-# result = pd.DataFrame(list(session.execute('select code from hangseng')))
-# codes = [code for code in result['code']]
-codes = ['2778','6823','1848','906','884']
+result = pd.DataFrame(list(session.execute('select code from hangseng')))
+codes = [code for code in result['code']]
 
 for code in codes:
     print('current code:', code)
