813053a1276533b365ec085b938b3bb9938b513b
matdwcy <1711837358@qq.com>
2020-02-11 13:01:39 +0800

    add rw_edgar

---
+++ rw_edgar/edgar.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+import io
+from cassandra.cluster import Cluster
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+server = ['13.229.248.211']
+cluster = Cluster(server)
+session = cluster.connect('scrapy')
+result = pd.DataFrame(list(session.execute("select * from edgar")))
+print(len(result))
+for res in result.index.values:
+    row_data = result.ix[res,result.columns.values].to_dict()
+    print(row_data['body'])
