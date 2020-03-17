44e0b092e204bf9abb950ff0222c12c57aba3aa1
LucasNobre <lucaas.sn@gmail.com>
2020-03-17 09:17:16 -0300

    Update document.py
    Removed item_seven_a fetching. Now creating a "test_file.txt" with the item_seven text. Possible fix to  selecting item_seven when item_seven_a_head_index is not created

--- edgar/edgar/spiders/document.py
+++ edgar/edgar/spiders/document.py
@@ -2,22 +2,15 @@
 import scrapy
 import re
 from scrapy.shell import inspect_response
-from edgar.items import DocumentItem
+from item7.items import Item7Item
 
 
-# The 'example' spider has all the code that is needed and the "parse" method can be
-# pasted to any callback of an spider which will give the document's html response.
-# Please change the method name so it doesn't change the behaviour of the default
-# parse method of the spider.
-# The "item_seven_final" and "item_seven_a_final" are lists that can be joined
-# if needed. They are clean and have all the information in the items excluding
-# the tables.
-class DocumentSpider(scrapy.Spider):
-    name = 'document'
-    start_urls = ['document_url']
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    start_urls = [r"file:///C:\Users\Pichau\Downloads\form10k.html"]
 
     def parse(self, response):
-        item = DocumentItem()
+        item = Item7Item()
         # Select all the tags that are in the body, only the first child of the <text>
         all_selectors = response.xpath('//text/*')
         # Select the item tags by their title
@@ -33,20 +26,18 @@ class DocumentSpider(scrapy.Spider):
             if len(item_8_head[i]) != 0:
                 item_8_head_index = i
         # Select the information we need
-        item_seven = all_selectors[item_seven_head_index:item_seven_a_head_index]
-        item_seven_a = all_selectors[item_seven_a_head_index:item_8_head_index]
+        if 'item_seven_a_head_index' in locals():
+            item_seven = all_selectors[item_seven_head_index:item_seven_a_head_index]
+        else:
+            item_seven = all_selectors[item_seven_head_index:item_8_head_index]
         # Remove tables from the results
         item_seven = item_seven.xpath('./font//text()').getall()
-        item_seven_a = item_seven_a.xpath('./font//text()').getall()
         # Remove page numbers
         item_seven_no_ints = [element.strip() for element in item_seven
                               if re.match(r'^-?\d+(?:\.\d+)?$', element.strip()) is None]
-        item_seven_a_no_ints = [element.strip() for element in item_seven_a
-                              if re.match(r'^-?\d+(?:\.\d+)?$', element.strip()) is None]
         # Remove blank list values
         item_seven_final = list(filter(None, item_seven_no_ints))
-        item_seven_a_final = list(filter(None, item_seven_a_no_ints))
-        # Please add items
+        with open('test_file.txt', 'w') as f:
+            f.write(r'\n'.join(item_seven_final))
         item['item_seven'] = item_seven_final
-        item['item_sevena'] = item_seven_a_final
         yield item
