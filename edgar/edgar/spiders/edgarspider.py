1d4215bf58d192788d4678d2438f2dbe2bcc9d50
matdwcy <1711837358@qq.com>
2020-03-18 12:55:53 +0800

    fix

--- edgar/edgar/spiders/edgarspider.py
+++ edgar/edgar/spiders/edgarspider.py
@@ -116,7 +116,7 @@ class EdgarspiderSpider(scrapy.Spider):
             # Remove tables from the results
             item_seven = item_seven.xpath('./font//text()').getall()
             # Remove page numbers
-            item_seven_no_ints = [element.strip().replace('\x93','"').replace('\x94',"'") for element in item_seven
+            item_seven_no_ints = [element.strip().decode('utf-8') for element in item_seven
                                   if re.match(r'^-?\d+(?:\.\d+)?$', element.strip()) is None]
             # Remove blank list values
             item_seven_final = list(filter(None, item_seven_no_ints))
