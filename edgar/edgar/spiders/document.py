c1447c8814a96abb2ad8b5026e17fba6ead8cc9c
LucasNobre <lucaas.sn@gmail.com>
2020-03-31 19:53:43 -0300

    Update document.py
    Updating working script. Now I am testing  the xpaths before using set::difference

--- edgar/edgar/spiders/document.py
+++ edgar/edgar/spiders/document.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import w3lib.html
 import re
 from scrapy.shell import inspect_response
 import pandas as pd
@@ -10,26 +11,44 @@ from item7.items import Item7Item
 class ExampleSpider(scrapy.Spider):
     name = 'example'
 
+    def clean_html(self, text) -> str:
+        # body_without_tables = w3lib.html.remove_tags_with_content(text, which_ones=('table',))
+        body_without_escape_chars = w3lib.html.replace_escape_chars(text)
+        cleaned_body = w3lib.html.strip_html5_whitespace(body_without_escape_chars)
+        replaced_entities = w3lib.html.replace_entities(cleaned_body)
+        return replaced_entities
+
     def start_requests(self):
-        urls = pd.read_excel('E:\Programacao\Scrapy\Dick Chan\item7\edgar.xlsx').body_url.tolist()
+        urls = pd.read_excel('E:\Programacao\Scrapy\Dick Chan\item7\edgar0324.xlsx').body_url.tolist()
         for url in urls:
             yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
         item = Item7Item()
-        item_7_possible_titles = ['DISCUSSION AND ANALYSIS OF', 'Discussion and Analysis of Financial']
-        item_8_possible_titles = ['FINANCIAL STATEMENTS AND SUPPLEMENTARY DATA', 'CONSOLIDATED FINANCIAL STATEMENTS',
+        item_seven = []
+        cleaned_body = self.clean_html(response.body)
+        # Removing the start of the document
+        start_of_document_index = int(len(cleaned_body)/20)
+        cleaned_body = cleaned_body[start_of_document_index:]
+        response = response.replace(body=cleaned_body)
+        item_7_possible_titles = ['Item 7.', 'ITEM 7.', 'DISCUSSION AND ANALYSIS OF',
+                                  'Discussion and Analysis of Financial']
+        item_8_possible_titles = ['Item 8.', 'ITEM 8.', 'FINANCIAL STATEMENTS AND SUPPLEMENTARY DATA',
+                                  'CONSOLIDATED FINANCIAL STATEMENTS',
                                   'Financial Statements and Supplementary Data', 'Consolidated Financial Statements']
         for item_seven_title in item_7_possible_titles:
             for item_8_title in item_8_possible_titles:
                 item_seven_xpath = f'text()[contains(., "{item_seven_title}")]'
                 item_8_xpath = f'text()[contains(., "{item_8_title}")]'
+                item_seven_xpath_to_diff = f'/*/*//{item_seven_xpath}/preceding::*'
+                item_8_xpath_to_diff = f'/*/*//{item_8_xpath}/preceding::*'
                 # I am selecting all the nodes before node 2 and all the nodes from node 1
                 # and removing the nodes of node 1
                 # from node 2. This way I am selecting all the information between items 7 and 8
-                item_seven = response.xpath(f'set:difference(/*/*//{item_8_xpath}/preceding::'
-                                            f'*, /*/*//{item_seven_xpath}/preceding::*)'
-                                            f'/*[not(ancestor-or-self::table)]//text()').getall()
+                if response.xpath(item_seven_xpath_to_diff) and response.xpath(item_8_xpath_to_diff):
+                    item_xpath = f'set:difference({item_8_xpath_to_diff}, {item_seven_xpath_to_diff})' \
+                                 f'/*[not(ancestor-or-self::table)]//text()'
+                    item_seven = response.xpath(item_xpath).getall()
                 if item_seven:
                     break
             if item_seven:
@@ -42,7 +61,7 @@ class ExampleSpider(scrapy.Spider):
         file_name = response.url.split('/')[-1].split('.')[0]
         # with open(f'text_files/{file_name}.txt', 'w') as f:
         #     f.write(r'\n'.join(item_seven_final))
-        is_item_seven =  1 if len(item_seven_final) < 1500 else 0
+        is_item_seven = 1 if len(item_seven_final) < 1500 else 0
         item['item_seven'] = item_seven_final
         item['is_item_seven'] = is_item_seven
         item['url'] = response.url
